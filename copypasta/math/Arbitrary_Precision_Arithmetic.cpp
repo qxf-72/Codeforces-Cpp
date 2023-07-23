7e8aacec2fabb0c511130b4b1109dd70009a87e3
XiaofengQiu <18320651165@163.com>
2023-07-23 22:58:15 +0800

    modified:   copypasta/math/Arbitrary_Precision_Arithmetic.cpp

--- copypasta/math/Arbitrary_Precision_Arithmetic.cpp
+++ copypasta/math/Arbitrary_Precision_Arithmetic.cpp
@@ -1,5 +1,5 @@
 /*
-    BigIntTiny类：
+    BigIntTiny类：https://github.com/qxf-72/Codeforces-Cpp/blob/main/copypasta/math/BigIntTiny.cpp
         - BigIntTiny类利用vector<int>存储数位，同时实现了压位，将4位压缩为1位存储
         - 具体操作方面，实现了大数之间的<,>,==,!=,<=,>=,*,/,%的操作
         - 在具体测试时，发现除法操作时间消耗极高，在需要进行除法操作时建议使用BigInt_simplest类
@@ -7,7 +7,7 @@
 
 
 /*
-    BigInt_simplest类：
+    BigInt_simplest类：https://github.com/qxf-72/Codeforces-Cpp/blob/main/copypasta/math/BigInt_simplest.cpp
         - 利用数组int [] 存储数位
         - 实现了大数和int的乘法，大数和int的除法，大数和int的<操作
         - 只能进行正数的运算
