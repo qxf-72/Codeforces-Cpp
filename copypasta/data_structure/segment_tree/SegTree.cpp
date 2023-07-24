9d8f466eb86310c1cb5a3d691800c84d4968e906
XiaofengQiu <18320651165@163.com>
2023-07-24 22:00:50 +0800

    Changes to be committed: 	modified:   copypasta/data_structure/segment_tree/SegTree.cpp 	modified:   copypasta/data_structure/segment_tree/SegTree_LazyRangeAdd.cpp 	modified:   copypasta/data_structure/segment_tree/SegTree_LazyRangeSet.cpp 	modified:   copypasta/data_structure/segment_tree/segment_tree.md

--- copypasta/data_structure/segment_tree/SegTree.cpp
+++ copypasta/data_structure/segment_tree/SegTree.cpp
@@ -1,58 +1,68 @@
-#include <string>
-#include <vector>
-#include <memory>
-
-using namespace std;
-
-//线段树模板，根据不同要求进行修改使用,可以进行的操作如下：
-//区间求和，区间最大值，区间最小值
-class SegmentTree{
+template<typename T>
+class SegmentTree {
 private:
-    static const int INF=0x3f3f3f3f;
-    vector<int> dat;
+    int n = 1;
+    vector<T> tree;
+    vector<T> *arr;
+
+    void build(int k) {
+        if (k >= n - 1) {
+            if (k - n + 1 < arr->size())
+                tree[k] = (*arr)[k - n + 1];
+            else {
+                // do something
+                // 由不会影响结果的值填充到空白节点
+            }
+            return;
+        }
+        build(2 * k + 1);
+        build(2 * k + 2);
+        // do something
+        //由tree[2*k+1]和tree[2*k+2]的值得到tree[k]
+    }
+
+    T query(int a, int b, int k, int l, int r) {
+        if (r <= a || b <= l) {
+            //return something
+            //返回一个不会影响结果的值
+        }
+        if (a <= l && b >= r) return tree[k];
+        T v_l = query(a, b, k * 2 + 1, l, (l + r) / 2);
+        T v_r = query(a, b, k * 2 + 2, (l + r) / 2, r);
+        //return something
+        //返回的值由左右两边得到
+    }
+
 public:
-    int n;
-    void init(int n_){
-        //n_为元素数量，但是为了便于处理，将其元素个数扩大到2的幂
-        n=1;
-        while(n<n_) n*=2;
-        dat.resize(2*n-1);
+    SegmentTree<T>() = default;
 
-        //不同要求，数组初始化也不同：
-        //区间求和 0 ；区间最小值 INF； 区间最大值：—INF
-        for(int i=0;i<2*n-1;++i) dat[i]=0;
+    explicit SegmentTree<T>(vector<T> *arr) : arr(arr) {
+        while (n < arr->size())
+            n *= 2;
+        tree.resize(2 * n - 1);
+        build(0);
     }
 
-    //把下标为k(从0开始)的值进行更新
-    void update(int k,int a){
-        k+=n-1;
-        dat[k]=a;
-        while(k>0){
-            k=(k-1)/2;
-            //不同要求，数组更新也不同：
-            //区间求和   :   dat[k]=dat[2*k+1]+dat[2*k+2];
-            //区间最小值 :   dat[k]=min(dat[2*k+1],dat[2*k+2]);
-            //区间最大值 :   dat[k]=max(dat[2*k+1],dat[2*k+2]);
-            dat[k]=dat[2*k+1]+dat[2*k+2];
+    void update(int k, T a) {
+        k += n - 1;
+        tree[k] = a;
+        while (k > 0) {
+            k = (k - 1) / 2;
+            // do something
+            // 由tree[2*k+1]和tree[2*k+2]的值得到tree[k]
         }
     }
 
-    //查询区间[a,b)的最小值
-    //k为当前区间对应的节点的下标,初始设为0
-    //[l,r)为当前区间，初始为[0,n)
-    int query(int a,int b,int k,int l,int r){
-        //区间求和   :   return 0;
-        //区间最小值 :   return INF;
-        //区间最大值 :   return -INF;
-        if(r<=a || b<=l) return 0;
-        if(a<=l && b>=r) return dat[k];
-        else{
-            int v_l=query(a,b,k*2+1,l,(l+r)/2);
-            int v_r=query(a,b,k*2+2,(l+r)/2,r);
-            //区间求和   :   return v_l+v_r;
-            //区间最小值 :   return min(v_l,v_r);
-            //区间最大值 :   return max(v_l,v_r);
-            return v_l+v_r;
-        }
+    T query(int a, int b) {
+        return query(a, b, 0, 0, n);
+    }
+
+    SegmentTree<T> &operator=(const SegmentTree<T> &segmentTree) {
+        if (this == &segmentTree)
+            return *this;
+        n = segmentTree.n;
+        arr = segmentTree.arr;
+        tree = std::move(segmentTree.tree);
+        return *this;
     }
 };
