83f1609f7d600cbcca6dd07f79607f83d9f25318
XiaofengQiu <18320651165@163.com>
2024-05-20 09:07:09 +0800

    feat: 更新ST表模板

--- copypasta/Basic_Algorithm/SparseTable.cpp
+++ copypasta/Basic_Algorithm/SparseTable.cpp
@@ -1,45 +1,39 @@
-template<typename T>
-class SparseTable
-{
-	using func_type = function<T(const T&, const T&)>;
+template <typename T>
+class SparseTable {
+    using func_type = function<T(const T &, const T &)>;
 
-	vector<vector<T>> ST;	// 稀疏表
-	vector<int> log_;		// 预处理k值
-	func_type op;			// 对元素的操作，默认为max()
-	static T default_func(const T& t1, const T& t2)
-	{
-		return max(t1, t2);
-	}
- public:
-	SparseTable(const vector<T>& a, func_type _func = default_func)
-	{
-		op = _func;
-		int len = a.size();
-		int l1 = ceil(log2(len)) + 1;
-		ST.assign(len, vector<T>(l1, 0));
-		
-		for (int i = 0; i < len; ++i)
-			ST[i][0] = a[i];
+    vector<vector<T>> St; // 稀疏表
+    vector<int> Log;      // 预处理k值
+    func_type op;         // 对元素的操作,默认为max()
 
-		for (int j = 1; j < l1; ++j)
-		{
-			int pj = (1 << j) - 1;
-			for (int i = 0; i + pj < len; ++i)
-			{
-				ST[i][j] = op(ST[i][j - 1], ST[i + (1 << (j - 1))][j - 1]);
-			}
-		}
+    static T default_func(const T &t1, const T &t2) {
+        return max(t1, t2);
+    }
 
-		log_.assign(len + 1, 0);
-		for (int i = 2; i <= len; ++i)
-			log_[i] = log_[i / 2] + 1;
+public:
+    SparseTable(const vector<T> &a, func_type _func = default_func) {
+        op = _func;
+        int len = a.size();
+        int len2 = (int)log2(len) + 1;
+        St.assign(len, vector<T>(len2, 0));
 
-	}
+        for (int i = 0; i < len; ++i)
+            St[i][0] = a[i];
 
-	T query(int l, int r)
-	{
-		int k = log_[r - l + 1];
-		return op(ST[l][k], ST[r - (1 << k) + 1][k]);
-	}
+        for (int j = 1; j < len2; ++j) {
+            int jlen = (1 << j) - 1;
+            for (int i = 0; i + jlen < len; ++i) {
+                St[i][j] = op(St[i][j - 1], St[i + (1 << (j - 1))][j - 1]);
+            }
+        }
 
+        Log.assign(len + 1, 0);
+        for (int i = 2; i <= len; ++i)
+            Log[i] = Log[i / 2] + 1;
+    }
+
+    T query(int l, int r) {
+        int k = Log[r - l + 1];
+        return op(St[l][k], St[r - (1 << k) + 1][k]);
+    }
 };
